d1ed5aa82ae8285cb10fa6366eb3cce3677c2194
Tyson Light <tysonwjl@gmail.com>
2022-09-01 21:51:13 +1000

    Small improvement to F & C, need to fix value bug

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
+
+
+// Todo
+// c and f degree icons, convert all values to required values
+// cap multimple word cities
+
+// Completed: Fixed opacity on load, Added auto capitalization to returned city
+
+
+
+
+// Global variables
 var type = "a";
 var type_two = "a";
 var times = 1;
 var wrapper = document.getElementsByClassName("wrapper");
-wrapper[0].style.maxHeight = "0px";
 var card = document.getElementsByClassName("card");
 var ftoc = document.getElementsByClassName('ftoc')
+
+// Styles on load
+wrapper[0].style.maxHeight = "0px";
 card[0].style.backgroundColor = "transparent";
+card[0].style.opacity = "0";
 
 
 //Once the DOM has loaded, we can start our script
@@ -48,11 +63,16 @@ document.addEventListener("DOMContentLoaded",function() {
             card = document.getElementsByClassName("card");
             // setTimeout(function() {wrapper[0].style.maxHeight = "2000px"}, 2000);
             wrapper[0].style.maxHeight = "2000px";
-
-            for (var i = 0; i <= card.length-1; i++) {
-                card[i].style.opacity = 1;
+            if(card.length != 1){
+                for (var i = 0; i <= card.length-1; i++) {
+                    card[i].style.opacity = 1;
+                }
+                type = "b";
             }
-            type = "b";
+            else{
+                card[0].style.opacity = 0
+                type = "b"
+            }
    
         } 
         else if (type == "b") {
@@ -67,7 +87,9 @@ document.addEventListener("DOMContentLoaded",function() {
             type = "a";
         }
     };
-        
+    // Declaring variables   
+    var high = 1;
+    var low = 1;
     //Get and show weather
     function getAndShowWeather() {
         var city_input = document.getElementsByName("city");
@@ -81,8 +103,34 @@ document.addEventListener("DOMContentLoaded",function() {
             
             var results = fetch(url).then(function(response) {
                 return response.json();}).then(function(text) {
-                    console.log('Request successful', text); 
-                    wrapper[0].insertAdjacentHTML('afterbegin', '<div class="day-mon card"><h2 class="city_name">'+ text.address+'</h2><hr><h3 class="high">High: '+text.days[0].tempmax +'</h3><h3 class="low">Low: '+text.days[0].tempmin +'</h3><i class="wi wi-day-sunny"></i><h2 class="descr">Mostly Sunny</div>');
+                    console.log('Request successful', text);
+                    var end_word = "";
+                    const input = text.address;
+                    const words = input.split(" ");
+                    
+                    for (let i = 0; i < words.length; i++) {
+                        words[i] = words[i][0].toUpperCase().substr(0,1) + words[i].substr(1);
+                        end_word += words[i] + " ";
+                        console.log(words[i]);
+                    }
+                    
+                    words.join(" ");
+
+                    
+                    // Fix repeating value bug
+
+                    high = text.days[0].tempmax;
+                    low = text.days[0].tempmin
+
+                    console.log(end_word);
+                    console.log(high);
+                    console.log(low);
+                    console.log(text.days.length);
+                    
+
+            
+
+                    wrapper[0].insertAdjacentHTML('afterbegin', '<div class="day-mon card"><h2 class="city_name">'+ end_word +'</h2><hr><div class="vals"><div class="group"><h3 class="high">High:<h3 class="val_h"> '+high+'<i>&deg;C</i></h3></h3></div><div class="group"><h3 class="low">Low:<h3 class="val_l"> '+low +'<i>&deg;C</i></h3></h3></div><i class="wi wi-day-sunny"></i></div><h2 class="descr">Mostly Sunny</div>');
                     wrapper[0].style.maxHeight = 1500 + "px";
                     show_button[0].innerHTML = "Hide";
                     if(times == 1 && card.length == 2){
@@ -106,11 +154,20 @@ document.addEventListener("DOMContentLoaded",function() {
     //Change visible values from C to F and vice versa
     function changeType() {
         if(type_two == "a") {;
-            this.innerHTML = "Change to F";
+            this.innerHTML = "&deg;F";
             type_two = "b";
+            var high_t = document.getElementsByClassName("val_h");
+            var low_t = document.getElementsByClassName("val_l");
+
+            console.log(high_t);
+            console.log(low_t);
+
+
+            // console.log(high[0].innerHTML);
+            // console.log(high);
         }
         else if (type_two == "b") {
-            this.innerHTML = "Change to C";
+            this.innerHTML = "&deg;C";
             type_two = "a";
         }
     }
